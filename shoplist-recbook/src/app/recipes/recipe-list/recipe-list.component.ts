9f11e977065371965d87128b1dfba0c4a3c2f35c
younes-labiod <youneslabiod@gmail.com>
2023-12-16 22:53:44 +0400

    Section 3 | Adding Content to the Recipes Components

--- shoplist-recbook/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shoplist-recbook/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrl: './recipe-list.component.css'
+  styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe',
+      'Test',
+      'https://www.slobodenpecat.mk/wp-content/uploads/2023/08/%D0%BF%D0%B5%D1%87%D0%B5%D0%BD-%D0%B7%D0%B5%D0%BB%D0%B5%D0%BD%D1%87%D1%83%D0%BA-%D1%81%D0%BE-%D1%98%D0%B0%D1%98%D1%86%D0%B0-%D0%A4%D0%BE%D1%82%D0%BE-therecipestuff-in.jpg'
+    ),
+  ];
 }
