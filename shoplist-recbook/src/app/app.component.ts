a3f0a0a2080a978ca7e400ac5832153ef85214d4
younes-labiod <youneslabiod@gmail.com>
2023-12-23 15:32:29 +0400

    Section 6 | Adding Navigation with Event Binding and ngIf

--- shoplist-recbook/src/app/app.component.ts
+++ shoplist-recbook/src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'shoplist-recbook';
+
+  componentName: string = 'recipe';
+
+  onNavigate(componentName: string) {
+    this.componentName = componentName;
+  }
 }
--- shoplist-recbook/src/app/header/header.component.ts
+++ shoplist-recbook/src/app/header/header.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  @Output() selectedComponent = new EventEmitter<string>();
+
+  onSelect(component: string) {
+    this.selectedComponent.emit(component);
+  }
+}
