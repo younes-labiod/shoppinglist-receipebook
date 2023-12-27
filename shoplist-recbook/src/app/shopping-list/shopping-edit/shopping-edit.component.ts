07f1a7acb64f94cdf2373109f90dcc7e130c7b2a
younes-labiod <youneslabiod@gmail.com>
2023-12-27 23:52:27 +0400

    Adding the shopping list service

--- shoplist-recbook/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ shoplist-recbook/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -6,6 +6,7 @@ import {
   ViewChild,
 } from '@angular/core';
 import { Ingredient } from '../../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -16,13 +17,14 @@ export class ShoppingEditComponent {
   @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
   @ViewChild('amountInput', { static: false }) amountInputRef: ElementRef;
 
-  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+  constructor(private shoppingList: ShoppingListService) {}
 
   onAddItem() {
     const newIngredient = new Ingredient(
       this.nameInputRef.nativeElement.value,
       this.amountInputRef.nativeElement.value
     );
-    this.ingredientAdded.emit(newIngredient);
+
+    this.shoppingList.addIngredient(newIngredient);
   }
 }
--- shoplist-recbook/src/app/shopping-list/shopping-list.component.ts
+++ shoplist-recbook/src/app/shopping-list/shopping-list.component.ts
@@ -1,18 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.css',
 })
-export class ShoppingListComponent {
-  ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomato', 10),
-  ];
+export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[];
 
-  onIngredientAdded(ingr: Ingredient) {
-    this.ingredients.push(ingr);
+  constructor(private shoppingList: ShoppingListService) {}
+
+  ngOnInit(): void {
+    this.ingredients = this.shoppingList.getIngredient();
   }
 }
